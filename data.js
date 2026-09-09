0bda64de13e9c2670630d07db86b2a79926206db
chujinhyo-cmyk <chu.jin.hyo@gmail.com>
2026-09-09 23:11:05 +0900

    Add files via upload

---
+++ data.js
@@ -0,0 +1,80 @@
+const memoriesData = [
+  {
+    "id": "post-2015-cambodia",
+    "year": 2015,
+    "date": "2015-05-15",
+    "title": "2015년 캄보디아 가족 여행",
+    "location": {
+      "name": "캄보디아 앙코르와트",
+      "lat": 13.4125,
+      "lng": 103.8670
+    },
+    "description": "온 가족이 함께 떠난 신비로운 앙코르와트와 캄보디아 탐방 기록 (사진 677장)",
+    "coverMedia": {
+      "id": "1zcuUSMj4N26zFNhxx2ylWFSSBckqM8La",
+      "type": "image",
+      "url": "https://lh3.googleusercontent.com/d/1zcuUSMj4N26zFNhxx2ylWFSSBckqM8La",
+      "thumbnailUrl": "https://lh3.googleusercontent.com/d/1zcuUSMj4N26zFNhxx2ylWFSSBckqM8La",
+      "name": "IMG_7440.JPG",
+      "createdDate": "2015-05-15"
+    },
+    "mediaList": [
+      {"id":"1zcuUSMj4N26zFNhxx2ylWFSSBckqM8La","type":"image","url":"https://lh3.googleusercontent.com/d/1zcuUSMj4N26zFNhxx2ylWFSSBckqM8La","thumbnailUrl":"https://lh3.googleusercontent.com/d/1zcuUSMj4N26zFNhxx2ylWFSSBckqM8La","name":"IMG_7440.JPG","createdDate":"2015-05-15"},
+      {"id":"1vrLPKp6WPUBCiFZfDFI9zGw1yLSdF0Hp","type":"image","url":"https://lh3.googleusercontent.com/d/1vrLPKp6WPUBCiFZfDFI9zGw1yLSdF0Hp","thumbnailUrl":"https://lh3.googleusercontent.com/d/1vrLPKp6WPUBCiFZfDFI9zGw1yLSdF0Hp","name":"IMG_7439.JPG","createdDate":"2015-05-15"},
+      {"id":"1Na995SiLNAEF82DlQZpEWJKM_f_gO0Wb","type":"image","url":"https://lh3.googleusercontent.com/d/1Na995SiLNAEF82DlQZpEWJKM_f_gO0Wb","thumbnailUrl":"https://lh3.googleusercontent.com/d/1Na995SiLNAEF82DlQZpEWJKM_f_gO0Wb","name":"IMG_7433.JPG","createdDate":"2015-05-15"},
+      {"id":"1HWIinlejPwekGAiADCAT6004rGAPq8IZ","type":"image","url":"https://lh3.googleusercontent.com/d/1HWIinlejPwekGAiADCAT6004rGAPq8IZ","thumbnailUrl":"https://lh3.googleusercontent.com/d/1HWIinlejPwekGAiADCAT6004rGAPq8IZ","name":"IMG_7428.JPG","createdDate":"2015-05-15"},
+      {"id":"1HpbBcCNycdtqj27TLTx5ZdW1yE-rXSzm","type":"image","url":"https://lh3.googleusercontent.com/d/1HpbBcCNycdtqj27TLTx5ZdW1yE-rXSzm","thumbnailUrl":"https://lh3.googleusercontent.com/d/1HpbBcCNycdtqj27TLTx5ZdW1yE-rXSzm","name":"IMG_7427.JPG","createdDate":"2015-05-15"},
+      {"id":"1aR8LvXbVXMFm0k3PInYzS_2-cI-tLWZ3","type":"image","url":"https://lh3.googleusercontent.com/d/1aR8LvXbVXMFm0k3PInYzS_2-cI-tLWZ3","thumbnailUrl":"https://lh3.googleusercontent.com/d/1aR8LvXbVXMFm0k3PInYzS_2-cI-tLWZ3","name":"IMG_7426.JPG","createdDate":"2015-05-15"},
+      {"id":"1zQpmhRSirqAOuDIrN28PlCbI2xGUB1ln","type":"image","url":"https://lh3.googleusercontent.com/d/1zQpmhRSirqAOuDIrN28PlCbI2xGUB1ln","thumbnailUrl":"https://lh3.googleusercontent.com/d/1zQpmhRSirqAOuDIrN28PlCbI2xGUB1ln","name":"IMG_7423.JPG","createdDate":"2015-05-15"},
+      {"id":"1cOo54wyt7uuxmuDT7-5YSyQiX3bv4pg_","type":"image","url":"https://lh3.googleusercontent.com/d/1cOo54wyt7uuxmuDT7-5YSyQiX3bv4pg_","thumbnailUrl":"https://lh3.googleusercontent.com/d/1cOo54wyt7uuxmuDT7-5YSyQiX3bv4pg_","name":"IMG_7408.JPG","createdDate":"2015-05-15"},
+      {"id":"1MsQTrNH2GJl1xFAwCvuUdIar1HZyrLf4","type":"image","url":"https://lh3.googleusercontent.com/d/1MsQTrNH2GJl1xFAwCvuUdIar1HZyrLf4","thumbnailUrl":"https://lh3.googleusercontent.com/d/1MsQTrNH2GJl1xFAwCvuUdIar1HZyrLf4","name":"IMG_7401.JPG","createdDate":"2015-05-15"},
+      {"id":"10Jnffo4flJ4qXsmjW_RIPzzs9GIAAJ44","type":"image","url":"https://lh3.googleusercontent.com/d/10Jnffo4flJ4qXsmjW_RIPzzs9GIAAJ44","thumbnailUrl":"https://lh3.googleusercontent.com/d/10Jnffo4flJ4qXsmjW_RIPzzs9GIAAJ44","name":"IMG_7398.JPG","createdDate":"2015-05-15"},
+      {"id":"1DonQlx6qOiBhnv2YrEw0nzuPCjXWtwZM","type":"image","url":"https://lh3.googleusercontent.com/d/1DonQlx6qOiBhnv2YrEw0nzuPCjXWtwZM","thumbnailUrl":"https://lh3.googleusercontent.com/d/1DonQlx6qOiBhnv2YrEw0nzuPCjXWtwZM","name":"IMG_8289.JPG","createdDate":"2015-05-15"},
+      {"id":"1kwUiu6GrcyF0vdFR5wxH5aJSPUD8PmkV","type":"image","url":"https://lh3.googleusercontent.com/d/1kwUiu6GrcyF0vdFR5wxH5aJSPUD8PmkV","thumbnailUrl":"https://lh3.googleusercontent.com/d/1kwUiu6GrcyF0vdFR5wxH5aJSPUD8PmkV","name":"IMG_8290.JPG","createdDate":"2015-05-15"},
+      {"id":"1LjQ-zky4JMt3MUgHgHTM-pxYROomkvRl","type":"image","url":"https://lh3.googleusercontent.com/d/1LjQ-zky4JMt3MUgHgHTM-pxYROomkvRl","thumbnailUrl":"https://lh3.googleusercontent.com/d/1LjQ-zky4JMt3MUgHgHTM-pxYROomkvRl","name":"IMG_8288.JPG","createdDate":"2015-05-15"},
+      {"id":"1hFwC_wvPOyNz4nkev57PyKUGrz2sTwaY","type":"image","url":"https://lh3.googleusercontent.com/d/1hFwC_wvPOyNz4nkev57PyKUGrz2sTwaY","thumbnailUrl":"https://lh3.googleusercontent.com/d/1hFwC_wvPOyNz4nkev57PyKUGrz2sTwaY","name":"IMG_8287.JPG","createdDate":"2015-05-15"},
+      {"id":"1kSLxOkQWu8rxqkyWPsW9FrW1ELN71AN3","type":"image","url":"https://lh3.googleusercontent.com/d/1kSLxOkQWu8rxqkyWPsW9FrW1ELN71AN3","thumbnailUrl":"https://lh3.googleusercontent.com/d/1kSLxOkQWu8rxqkyWPsW9FrW1ELN71AN3","name":"IMG_8286.JPG","createdDate":"2015-05-15"},
+      {"id":"1hLwe_mXPvrllc3NinnOY6yT0FTVGd7gf","type":"image","url":"https://lh3.googleusercontent.com/d/1hLwe_mXPvrllc3NinnOY6yT0FTVGd7gf","thumbnailUrl":"https://lh3.googleusercontent.com/d/1hLwe_mXPvrllc3NinnOY6yT0FTVGd7gf","name":"IMG_8285.JPG","createdDate":"2015-05-15"},
+      {"id":"1CdyCnhLzQ2Ms4H3dWw2eHWx6IcA6DrcN","type":"image","url":"https://lh3.googleusercontent.com/d/1CdyCnhLzQ2Ms4H3dWw2eHWx6IcA6DrcN","thumbnailUrl":"https://lh3.googleusercontent.com/d/1CdyCnhLzQ2Ms4H3dWw2eHWx6IcA6DrcN","name":"IMG_8283.JPG","createdDate":"2015-05-15"},
+      {"id":"1GzhitagDTuL2TDIWgpSTvQzO3q1nyIVv","type":"image","url":"https://lh3.googleusercontent.com/d/1GzhitagDTuL2TDIWgpSTvQzO3q1nyIVv","thumbnailUrl":"https://lh3.googleusercontent.com/d/1GzhitagDTuL2TDIWgpSTvQzO3q1nyIVv","name":"IMG_8284.JPG","createdDate":"2015-05-15"},
+      {"id":"1SWetBqPaoPWu_Rmde5nKhhEOCZzaTQ5O","type":"image","url":"https://lh3.googleusercontent.com/d/1SWetBqPaoPWu_Rmde5nKhhEOCZzaTQ5O","thumbnailUrl":"https://lh3.googleusercontent.com/d/1SWetBqPaoPWu_Rmde5nKhhEOCZzaTQ5O","name":"IMG_8281.JPG","createdDate":"2015-05-15"},
+      {"id":"1sSFBwTNePnCrwj90lmJRVhegAaMU3tqT","type":"image","url":"https://lh3.googleusercontent.com/d/1sSFBwTNePnCrwj90lmJRVhegAaMU3tqT","thumbnailUrl":"https://lh3.googleusercontent.com/d/1sSFBwTNePnCrwj90lmJRVhegAaMU3tqT","name":"IMG_8282.JPG","createdDate":"2015-05-15"},
+      {"id":"1V6DAozD6yPvyokw14E8Q1mABAbo8zZHw","type":"image","url":"https://lh3.googleusercontent.com/d/1V6DAozD6yPvyokw14E8Q1mABAbo8zZHw","thumbnailUrl":"https://lh3.googleusercontent.com/d/1V6DAozD6yPvyokw14E8Q1mABAbo8zZHw","name":"IMG_8280.JPG","createdDate":"2015-05-15"},
+      {"id":"17jAUn2Cak1G15wLxRQPpDpE7zb3GWtHN","type":"image","url":"https://lh3.googleusercontent.com/d/17jAUn2Cak1G15wLxRQPpDpE7zb3GWtHN","thumbnailUrl":"https://lh3.googleusercontent.com/d/17jAUn2Cak1G15wLxRQPpDpE7zb3GWtHN","name":"IMG_8279.JPG","createdDate":"2015-05-15"}
+    ]
+  },
+  {
+    "id": "post-2015-spring",
+    "year": 2015,
+    "date": "2015-03-29",
+    "title": "2015년 따뜻한 봄날의 일상",
+    "location": {
+      "name": "김해 나들이",
+      "lat": 35.2285,
+      "lng": 128.8894
+    },
+    "description": "포근한 봄 햇살 아래 함께한 일상과 소중한 추억들 (사진 88장)",
+    "coverMedia": {
+      "id": "1VYiDcicumar74sH4a2_dE3c3hXB9b-kS",
+      "type": "image",
+      "url": "https://lh3.googleusercontent.com/d/1VYiDcicumar74sH4a2_dE3c3hXB9b-kS",
+      "thumbnailUrl": "https://lh3.googleusercontent.com/d/1VYiDcicumar74sH4a2_dE3c3hXB9b-kS",
+      "name": "P20150329_143533407.JPG",
+      "createdDate": "2015-03-29"
+    },
+    "mediaList": [
+      {"id":"1VYiDcicumar74sH4a2_dE3c3hXB9b-kS","type":"image","url":"https://lh3.googleusercontent.com/d/1VYiDcicumar74sH4a2_dE3c3hXB9b-kS","thumbnailUrl":"https://lh3.googleusercontent.com/d/1VYiDcicumar74sH4a2_dE3c3hXB9b-kS","name":"P20150329_143533407.JPG","createdDate":"2015-03-29"},
+      {"id":"1iF10Ci3INb1nHXixeSUH5gsDbvNNwPsy","type":"image","url":"https://lh3.googleusercontent.com/d/1iF10Ci3INb1nHXixeSUH5gsDbvNNwPsy","thumbnailUrl":"https://lh3.googleusercontent.com/d/1iF10Ci3INb1nHXixeSUH5gsDbvNNwPsy","name":"P20150329_143531706.JPG","createdDate":"2015-03-29"},
+      {"id":"1LRMTKffJMILLJ3-TuRfqq9SE6u6t_tmR","type":"image","url":"https://lh3.googleusercontent.com/d/1LRMTKffJMILLJ3-TuRfqq9SE6u6t_tmR","thumbnailUrl":"https://lh3.googleusercontent.com/d/1LRMTKffJMILLJ3-TuRfqq9SE6u6t_tmR","name":"P20150329_143712770.JPG","createdDate":"2015-03-29"},
+      {"id":"19PqVgf4vQjygxggSADv6QcdgdkEgzG9p","type":"image","url":"https://lh3.googleusercontent.com/d/19PqVgf4vQjygxggSADv6QcdgdkEgzG9p","thumbnailUrl":"https://lh3.googleusercontent.com/d/19PqVgf4vQjygxggSADv6QcdgdkEgzG9p","name":"P20150329_143711478.JPG","createdDate":"2015-03-29"},
+      {"id":"1ZvuLPGFImAsqRjKItuen_ACvaVm9OYOy","type":"image","url":"https://lh3.googleusercontent.com/d/1ZvuLPGFImAsqRjKItuen_ACvaVm9OYOy","thumbnailUrl":"https://lh3.googleusercontent.com/d/1ZvuLPGFImAsqRjKItuen_ACvaVm9OYOy","name":"P20150329_143524974.JPG","createdDate":"2015-03-29"},
+      {"id":"1s3mM_niTgPUUXoiStVAuG_vOfhrxD8HD","type":"image","url":"https://lh3.googleusercontent.com/d/1s3mM_niTgPUUXoiStVAuG_vOfhrxD8HD","thumbnailUrl":"https://lh3.googleusercontent.com/d/1s3mM_niTgPUUXoiStVAuG_vOfhrxD8HD","name":"P20150329_142725312.JPG","createdDate":"2015-03-29"},
+      {"id":"1LJOx5FbXLC-UqE0sQbL0llqv3Ohs4oMF","type":"image","url":"https://lh3.googleusercontent.com/d/1LJOx5FbXLC-UqE0sQbL0llqv3Ohs4oMF","thumbnailUrl":"https://lh3.googleusercontent.com/d/1LJOx5FbXLC-UqE0sQbL0llqv3Ohs4oMF","name":"P20150329_142724444.JPG","createdDate":"2015-03-29"},
+      {"id":"18CfLU0sSyOWoB65CGY0SATdZI_HQ_o4h","type":"image","url":"https://lh3.googleusercontent.com/d/18CfLU0sSyOWoB65CGY0SATdZI_HQ_o4h","thumbnailUrl":"https://lh3.googleusercontent.com/d/18CfLU0sSyOWoB65CGY0SATdZI_HQ_o4h","name":"P20150329_142721079.JPG","createdDate":"2015-03-29"},
+      {"id":"1XvwMrcipt6SJyrGWuSl1S7lClzZQQ7Qq","type":"image","url":"https://lh3.googleusercontent.com/d/1XvwMrcipt6SJyrGWuSl1S7lClzZQQ7Qq","thumbnailUrl":"https://lh3.googleusercontent.com/d/1XvwMrcipt6SJyrGWuSl1S7lClzZQQ7Qq","name":"P20150329_142711824.JPG","createdDate":"2015-03-29"},
+      {"id":"1Zp0z7CYLxGuD3ie65-dGmazXBiAC_qfr","type":"image","url":"https://lh3.googleusercontent.com/d/1Zp0z7CYLxGuD3ie65-dGmazXBiAC_qfr","thumbnailUrl":"https://lh3.googleusercontent.com/d/1Zp0z7CYLxGuD3ie65-dGmazXBiAC_qfr","name":"P20150329_142709326.JPG","createdDate":"2015-03-29"},
+      {"id":"1NSMnNpHf09PYXtJSkm2g6-n6D_7i5SAo","type":"image","url":"https://lh3.googleusercontent.com/d/1NSMnNpHf09PYXtJSkm2g6-n6D_7i5SAo","thumbnailUrl":"https://lh3.googleusercontent.com/d/1NSMnNpHf09PYXtJSkm2g6-n6D_7i5SAo","name":"P20150329_142707805.JPG","createdDate":"2015-03-29"},
+      {"id":"1SvJNJh0xCyKU9gjcxIKlA4WNWrnDFySi","type":"image","url":"https://lh3.googleusercontent.com/d/1SvJNJh0xCyKU9gjcxIKlA4WNWrnDFySi","thumbnailUrl":"https://lh3.googleusercontent.com/d/1SvJNJh0xCyKU9gjcxIKlA4WNWrnDFySi","name":"P20150329_142657923.JPG","createdDate":"2015-03-29"}
+    ]
+  }
+];
